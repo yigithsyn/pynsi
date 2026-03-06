07cedc0628b654897ed07b906bce4179bdebf06a
Hüseyin <yigit.hsyn@gmail.com>
2026-03-06 14:58:48 +0300

    1.0.3: fix imports

--- src/pynsi/__init__.py
+++ src/pynsi/__init__.py
@@ -2,3 +2,4 @@ from .Measurement import Measurement, Script
 from .BeamTable import BeamTable
 from .NearField import NearField
 from .FarField import FarField, ProbeType, OEWGProbeType, DisplayFormat
+from .Script import Script
